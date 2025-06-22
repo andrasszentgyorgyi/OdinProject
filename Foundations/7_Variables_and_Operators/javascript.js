f3f0ddb52072aec8b15bc5d37f2c7585e036725e
andrasszentgyorgyi <181062883+andrasszentgyorgyi@users.noreply.github.com>
2025-06-22 17:57:17 +0200

    Completed the Variables and Operators module.

---
+++ Foundations/7_Variables_and_Operators/javascript.js
@@ -0,0 +1,17 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age=11;
+console.log(age);
+
+const pi = 3.14;
+//pi = 10;
+console.log(pi);
+console.log((3+2)-76 *(1 +1));
+
+console.log(23+97);
+console.log(23+93+95+39+33+35);
+console.log((4+6+9)/77);
