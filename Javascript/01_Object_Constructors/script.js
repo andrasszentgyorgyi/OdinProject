5437cf1aecaae3a2f7e819229e2840a87dea7447
andrasszentgyorgyi <181062883+andrasszentgyorgyi@users.noreply.github.com>
2026-03-10 10:59:24 +0100

    Webpack practice lesson

--- Javascript/01_Object_Constructors/script.js
+++ Javascript/01_Object_Constructors/script.js
@@ -1,16 +1,13 @@
 function Book(title,author,pages,read){
     if(!new.target){
-        throw Error("You must use the 'new operator to call the constructor of Book!");
+        throw Error("You must use the 'new' operator to call the constructor of Book!");
     }
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        if(read == true){
-            console.log(`${title} by ${author}, ${pages}, read`);
-        } else {
-            console.log(`${title} by ${author}, ${pages}, not read yet`);
-        }
+        const readStatus = this.read ? "read" : "not read yet";
+        return(`${title} by ${author}, ${pages}, ${readStatus}`);
     };
 }
